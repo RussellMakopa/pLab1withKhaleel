e51cb1dd4293eca76c7c0d643424c8e400d4dcfd
Russel Makopa <rmakopa26@wooster.edu>
2025-02-19 13:34:01 -0500

    plab1 changes

--- plab1.py
+++ plab1.py
@@ -14,3 +14,22 @@ def load_data():
             return json.load(file)
     except FileNotFoundError:
         return {}
+
+
+def save_data(data):
+    with open(DATA_FILE, "w") as file:
+        json.dump(data, file, indent=4)
+
+def generate_short_id(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
+
+
+def is_valid_url(url):
+    regex = re.compile(
+    r'^(https://)?'
+    r'([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}'
+    r'(:[0-9]{1,5})?'
+    )
+    return re.match(regex, url)
+
